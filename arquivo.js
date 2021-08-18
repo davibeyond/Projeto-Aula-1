6c734eb2543217a603baeecafc0ed3b4835522d7
Davi Moura <davi.moura@beyondcompany.com.br>
2021-08-18 13:55:04 -0300

    promises funcionando

--- arquivo.js
+++ arquivo.js
@@ -1,20 +1,20 @@
-function fabricarPessoa(nomeDaPessoa, sobrenomeDaPessoa, dataDeNascimentoDaPessoa) {
-    return {
-        nome: nomeDaPessoa,
-        sobrenome: sobrenomeDaPessoa,
-        dataDeNascimento: dataDeNascimentoDaPessoa
-    }
+function funcaoSuperCustosa() {
+    return new Promise(function (resolve, reject) {
+        // vai rodar de forma assíncrona
+        const resultado = 10 + 5
+        resolve(resultado)
+    })
 }
 
-const pessoa1 = fabricarPessoa('Levi', 'Nobrega', 10)
-const pessoa2 = fabricarPessoa('Davi', 'Moura', 20)
-const pessoa3 = fabricarPessoa('Arthur', 'Lapprand', 30)
-const pessoa4 = fabricarPessoa('Paulo', 'Filho', 15)
-const pessoa5 = fabricarPessoa('Nathan', 'Martins', 25)
-const pessoas = [pessoa1, pessoa2, pessoa3, pessoa4, pessoa5]
-
-function lerPessoa(pessoa) {
-    console.log("Dentro do for lemos", pessoa)
+function esperar(segundos) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve, segundos * 1000)
+    })
 }
-pessoas.forEach(lerPessoa)
-console.log('Fim')
+
+async function principal() {
+    await esperar(5)
+    console.log('Passou-se 5 segundos!')
+}
+
+principal()
